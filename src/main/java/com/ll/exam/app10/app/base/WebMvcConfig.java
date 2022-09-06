850c4dbc2b61a1351a4635058e200926dbd0fba1
Ju, Minji <alswl0w0@gmail.com>
2022-09-06 13:49:35 +0900

    chore : 업로드한 파일을 웹서버에서 접근 가능 하도록
    *특정 경로로 들어갔을 때 업로드한 파일이 보이도록 핸들러 구현

---
+++ src/main/java/com/ll/exam/app10/app/base/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.ll.exam.app10.app.base;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Value("${custom.genFileDirPath}")
+    private String genFileDirPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/gen-file/**") // 이렇게 요청이 들어오면 ex. http://localhost:8010/gen-file/1.png
+                .addResourceLocations("file:///" + genFileDirPath + "/"); // 파일을 이 경로(genFileDirPath)에서 찾는다
+    }
+}
