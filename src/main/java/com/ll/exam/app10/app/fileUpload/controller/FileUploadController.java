bb7294fa0bfbc620638bdcb6bcb90be4ea8740bf
Ju, Minji <alswl0w0@gmail.com>
2022-09-06 13:31:55 +0900

    test : postman 테스트용으로 csrf 토큰 체크 꺼놓기

--- src/main/java/com/ll/exam/app10/app/fileUpload/controller/FileUploadController.java
+++ src/main/java/com/ll/exam/app10/app/fileUpload/controller/FileUploadController.java
@@ -1,6 +1,7 @@
 package com.ll.exam.app10.app.fileUpload.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
