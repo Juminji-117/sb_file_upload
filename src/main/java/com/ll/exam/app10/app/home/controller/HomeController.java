bfb4dc1df3c489953571f57f91042b2fa65ee485
Ju, Minji <alswl0w0@gmail.com>
2022-09-24 23:24:43 +0900

    refactor : 컨트롤러에서 현재 로그인한 회원정보 얻는 기존방법 (memberContext)

--- src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
@@ -1,8 +1,10 @@
 package com.ll.exam.app10.app.home.controller;
 
 import com.ll.exam.app10.app.member.service.MemberService;
+import com.ll.exam.app10.app.security.dto.MemberContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,10 +26,24 @@ public class HomeController {
         return "home/about";
     }
 
-    @PreAuthorize("isAuthenticated()")
+    // 회원정보 리턴하는 기존방법
     @GetMapping("/currentUserOrigin")
     @ResponseBody
     public Principal currentUserOrigin(Principal principal) {
         return principal;
+    }
+
+
+    // 회원정보 리턴하는 새로운 방법
+    @GetMapping("/currentUser")
+    @ResponseBody
+    public MemberContext currentUser(@AuthenticationPrincipal MemberContext memberContext) { // @AuthenticationPrincipal 어노테이션 붙이면 memberContext 바로 가져올 수 있음
+        return memberContext;
+        /*
+        ** MemberContext에서 얻을 수 있는 것
+        * memberContext.getUserName() -> 기존 Principal만으로도 가능
+        * memberContext.getId() -> MemberContext에서 새로 추가
+        * memberContext.getProfileImgUrl() -> MemberContext에서 새로 추가
+         */
     }
 }
