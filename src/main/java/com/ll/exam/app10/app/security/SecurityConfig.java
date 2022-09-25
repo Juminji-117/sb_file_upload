f35f54c4e7ae7e0afb0042dc628017cf731811cb
Ju Minji <alswl0w0@gmail.com>
2022-09-25 18:10:25 +0900

    chore : SecurityConfig에 OAuth2UserService 객체 등록 및 OAuth2 로그인 활성화

--- src/main/java/com/ll/exam/app10/app/security/SecurityConfig.java
+++ src/main/java/com/ll/exam/app10/app/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app10.app.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,6 +17,8 @@ import org.springframework.security.web.SecurityFilterChain;
 // 이걸 어노테이션을 붙여야 @PreAuthorize, @RolesAllowed 등의 어노테이션 사용 가능
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    @Autowired
+    private OAuth2UserService oAuth2UserService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -31,6 +35,15 @@ public class SecurityConfig {
                                 .loginPage("/member/login") // GET
                                 .loginProcessingUrl("/member/login") // POST
                 )
+                                .oauth2Login(
+                                        oauth2Login -> oauth2Login
+                                                .loginPage("/member/login")
+                                                .userInfoEndpoint(
+                                                        userInfoEndpoint -> userInfoEndpoint
+                                                                .userService(oAuth2UserService)
+                                                )
+                                )
+
                                 .logout(logout -> logout
                                         .logoutUrl("/member/logout")
                 );
