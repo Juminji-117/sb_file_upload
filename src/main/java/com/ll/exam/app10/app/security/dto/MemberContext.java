974e89c3c91bf89b84d994f643813e30485b7229
Ju Minji <alswl0w0@gmail.com>
2022-09-25 18:27:34 +0900

    feat : MemberContext가 OAuthUser 인터페이스 상속하여 OAuthUser2Service 사용
    *카카오 소셜로그인 구현완료

--- src/main/java/com/ll/exam/app10/app/security/dto/MemberContext.java
+++ src/main/java/com/ll/exam/app10/app/security/dto/MemberContext.java
@@ -4,13 +4,19 @@ import com.ll.exam.app10.app.member.entity.Member;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 // 스프링 시큐리티 기본 제공 클래스 User -> userName, password, authorities 제공
 // MemberContext 클래스를 새로 만들어 User 클래스를 상속받은 후 id와 profileImgUrl도 추가(스프링 시큐리티 기본 제공X)
-public class MemberContext extends User {
+public class MemberContext extends User implements OAuth2User {
+    private Map<String, Object> attributes;
+    private String userNameAttributeName;
     private final Long id;
     private final String email;
     private final String profileImgUrl;
@@ -21,5 +27,26 @@ public class MemberContext extends User {
         this.id = member.getId();
         this.email = member.getEmail();
         this.profileImgUrl = member.getProfileImgUrl();
+    }
+
+    public MemberContext(Member member, List<GrantedAuthority> authorities, Map<String, Object> attributes, String userNameAttributeName) {
+        this(member, authorities);
+        this.attributes = attributes;
+        this.userNameAttributeName = userNameAttributeName;
+    }
+
+    @Override
+    public Set<GrantedAuthority> getAuthorities() {
+        return super.getAuthorities().stream().collect(Collectors.toSet());
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attributes;
+    }
+
+    @Override
+    public String getName() {
+        return this.getAttribute(this.userNameAttributeName).toString();
     }
 }
